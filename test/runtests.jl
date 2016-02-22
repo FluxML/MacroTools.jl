fd302481990a1dc96698f22eebda0211015b462f
Mike Innes <mike.j.innes@gmail.com>
2016-02-22 11:58:48 +0000

    add more core tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,47 @@
 using MacroTools
 using Base.Test
 
+let
+  x = @match :(2+3) begin
+    (a_+b_) => (a, b)
+    (a_-b_) => (b, a)
+  end
+  @test x == (2, 3)
+end
+
+let
+  x = @match :(2-3) begin
+    (a_+b_) => (a, b)
+    (a_-b_) => (b, a)
+  end
+  @test x == (3, 2)
+end
+
+let
+  x = @match :(2/3) begin
+    (a_+b_) => (a, b)
+    (a_-b_) => (b, a)
+  end
+  @test x == nothing
+end
+
+let
+  x = @match :(2/3) begin
+    (a_+b_) => (a, b)
+    (a_-b_) => (b, a)
+    _ => :default
+  end
+  @test x == :default
+end
+
+let
+  ex = :(type Foo
+    x::Int
+    y
+  end)
+  @capture(ex, type T_ fields__ end)
+  @test T == :Foo
+  @test fields == [:(x::Int), :y]
+end
+
 include("destruct.jl")
