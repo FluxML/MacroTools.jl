d77f193591af724fa0c060d415061893fe5a8282
Yichao Yu <yyc1992@gmail.com>
2017-02-09 23:48:36 -0500

    Fix depwarns and errors on 0.6

--- test/destruct.jl
+++ test/destruct.jl
@@ -8,15 +8,23 @@ d = @destruct [a, b] = Dict(:a => 1, :b => 2)
 @destruct [foo = :a || 5, b = :b || 6, c || 7] = Dict(:a => 1)
 @test (foo, b, c) == (1, 6, 7)
 
-@destruct _.data = "foo"
-@test _ == "foo"
-@test data == "foo".data
+import Base: ==
+
+immutable S
+    data::Vector{UInt8}
+    S(s) = new(Vector{UInt8}(s))
+end
+==(s1::S, s2::S) = s1.data == s2.data
+
+@destruct x.data = S("foo")
+@test x == S("foo")
+@test data == S("foo").data
 
 @destruct _.(re, im) = Complex(1,2)
 @test (re, im) == (1, 2)
 
-@destruct [s.data = :a] = Dict(:a => "foo")
-@test s == "foo"
+@destruct [s.data = :a] = Dict(:a => S("foo"))
+@test s == S("foo")
 @test data === s.data
 
 @destruct x[a, [c, d] = b] = Dict(:a => 1, :b => Dict(:c => 2, :d => 3))
