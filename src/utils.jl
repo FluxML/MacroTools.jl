69f5811d984e61df016acc085e7881d022ff8a8e
Cedric St-Jean <cedric.stjean@gmail.com>
2017-08-11 13:55:28 -0400

    Handle nested where clauses in splitdef

--- src/utils.jl
+++ src/utils.jl
@@ -184,6 +184,17 @@ function shortdef1(ex)
 end
 shortdef(ex) = prewalk(shortdef1, ex)
 
+""" `gather_wheres(:(f(x::T, y::U) where T where U)) => (:(f(x::T, y::U)), (:U, :T))`
+"""
+function gather_wheres(ex)
+  if @capture(ex, (f_ where {params1__}))
+    f2, params2 = gather_wheres(f)
+    (f2, (params1..., params2...))
+  else
+    (ex, ())
+  end
+end
+
 doc"""    splitdef(fdef)
 
 Match any function definition
@@ -207,23 +218,23 @@ all_params = [get(dict, :params, [])..., get(dict, :whereparams, [])...]
 ```
 """
 function splitdef(fdef)
-    error_msg = "Not a function definition: $fdef"
-    @assert(@capture(longdef1(fdef),
-                     function ((fcall_ where {whereparams__}) | fcall_)
-                     body_ end),
-            error_msg)
-    @assert(@capture(fcall, ((func_(args__; kwargs__)) |
-                             (func_(args__; kwargs__)::rtype_) |
-                             (func_(args__)) |
-                             (func_(args__)::rtype_))),
-            error_msg)
-    @assert(@capture(func, (fname_{params__} | fname_)), error_msg)
-    di = Dict(:name=>fname, :args=>args,
-              :kwargs=>(kwargs===nothing ? [] : kwargs), :body=>body)
-    if rtype !== nothing; di[:rtype] = rtype end
-    if whereparams !== nothing; di[:whereparams] = whereparams end
-    if params !== nothing; di[:params] = params end
-    di
+  error_msg = "Not a function definition: $fdef"
+  @assert(@capture(longdef1(fdef),
+                   function (fcall_ | fcall_) body_ end),
+          "Not a function definition: $fdef")
+  fcall_nowhere, whereparams = gather_wheres(fcall)
+  @assert(@capture(fcall_nowhere, ((func_(args__; kwargs__)) |
+                                   (func_(args__; kwargs__)::rtype_) |
+                                   (func_(args__)) |
+                                   (func_(args__)::rtype_))),
+          error_msg)
+  @assert(@capture(func, (fname_{params__} | fname_)), error_msg)
+  di = Dict(:name=>fname, :args=>args,
+            :kwargs=>(kwargs===nothing ? [] : kwargs), :body=>body)
+  if rtype !== nothing; di[:rtype] = rtype end
+  if whereparams !== nothing; di[:whereparams] = whereparams end
+  if params !== nothing; di[:params] = params end
+  di
 end
 
 
