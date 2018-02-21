cc89618bcee7c1636d2724796385c5c75f60c499
Mike J Innes <mike.j.innes@gmail.com>
2018-02-21 23:01:02 +0000

    fixes #63

--- src/examples/forward.jl
+++ src/examples/forward.jl
@@ -1,3 +1,15 @@
+"""
+    @forward Foo.bar f, g, h
+
+`@forward` simply forwards method definition to a given field of a struct.
+For example, the above is  equivalent to
+
+```julia
+f(x::Foo, args...) = f(x.bar, args...)
+g(x::Foo, args...) = g(x.bar, args...)
+h(x::Foo, args...) = h(x.bar, args...)
+```
+"""
 macro forward(ex, fs)
   @capture(ex, T_.field_) || error("Syntax: @forward T.x f, g, h")
   T = esc(T)
