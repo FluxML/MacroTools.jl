0ff03ac5dc26e5ff1d5dfa63ef17d372c4be4781
Robin Deits <robin.deits@gmail.com>
2018-03-02 16:53:18 -0500

    another test

--- test/runtests.jl
+++ test/runtests.jl
@@ -43,6 +43,10 @@ let
   @capture(ex, type T_ fields__ end)
   @test T == :Foo
   @test fields == [:(x::Int), :y]
+  
+  @capture(ex, mutable struct T_ fields__ end)
+  @test T == :Foo
+  @test fields == [:(x::Int), :y]
 end
 
 let
