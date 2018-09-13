f6a4fa915ca3f99be85303c08d4c40c21cba5401
Mike J Innes <mike.j.innes@gmail.com>
2018-09-14 00:49:41 +0100

    organise

---
+++ test/match.jl
@@ -0,0 +1,90 @@
+let
+  x = @match :(2+3) begin
+    (a_+b_) => (a, b)
+    (a_-b_) => (b, a)
+  end
+  @test x == (2, 3)
+end
+
+let
+  x = @match :(2-3) begin
+    (a_+b_) => (a, b)
+    (a_-b_) => (b, a)
+  end
+  @test x == (3, 2)
+end
+
+let
+  x = @match :(2/3) begin
+    (a_+b_) => (a, b)
+    (a_-b_) => (b, a)
+  end
+  @test x == nothing
+end
+
+let
+  x = @match :(2/3) begin
+    (a_+b_) => (a, b)
+    (a_-b_) => (b, a)
+    _ => :default
+  end
+  @test x == :default
+end
+
+let
+  ex = :(mutable struct Foo
+           x::Int
+           y
+         end)
+  @capture(ex, mutable struct T_ fields__ end)
+  @test T == :Foo
+  @test fields == [:(x::Int), :y]
+
+  @capture(ex, mutable struct T_ fields__ end)
+  @test T == :Foo
+  @test fields == [:(x::Int), :y]
+end
+
+let
+  ex = :(f(x))
+  @capture(ex, f_(xs__))
+  @test f == :f
+  @test xs == [:x]
+end
+
+let
+  ex = :(f(x, y, z))
+  @capture(ex, f_(x_, xs__))
+  @test f == :f
+  @test x == :x
+  @test xs == [:y, :z]
+end
+
+let
+  ex = quote
+    function foo(a, b)
+      return a+b
+    end
+  end
+  @assert @capture(shortdef(ex), f_(args__) = body_)
+end
+
+let
+  ex = :(a = b)
+  @capture(ex, a_ = b_)
+  @test (a, b) == (:a, :b)
+end
+
+let
+  ex = :(f(a = b))
+  @capture(ex, f(a_ = b_))
+  @test (a, b) == (:a, :b)
+  @capture(ex, f(x_))
+  @test isexpr(x, :kw)
+end
+
+let
+  ex = :(@foo(a,b))
+  @capture(ex, @foo(a_,b_))
+  @test (a, b) == (:a, :b)
+end
