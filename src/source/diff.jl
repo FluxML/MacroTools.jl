b4b0885d464d1d07527fd9644c6917f20d1ffb69
Mike J Innes <mike.j.innes@gmail.com>
2018-09-17 01:20:43 +0100

    improve perf

--- src/source/diff.jl
+++ src/source/diff.jl
@@ -58,32 +58,40 @@ label(ex::Expr) = Expr(ex.head)
 children(ex) = []
 children(ex::Expr) = ex.args
 
-function delete(f1, f2)
+struct DiffCtx
+  idx::Vector{Int}
+  cache::Dict{Any,Patch}
+end
+
+DiffCtx() = DiffCtx([],IdDict())
+
+function delete(cx::DiffCtx, f1, f2)
   isempty(f1) && return
-  fdiff(f1[2:end], f2) + Patch([Delete(f1[1])])
+  fdiff(cx, f1[2:end], f2) + Patch([Delete(f1[1])])
 end
 
-function insert(f1, f2)
+function insert(cx::DiffCtx, f1, f2)
   isempty(f2) && return
-  fdiff(f1, f2[2:end]) + Patch([Insert(f2[1])])
+  fdiff(cx, f1, f2[2:end]) + Patch([Insert(f2[1])])
 end
 
-function replace(f1, f2)
+function replace(cx::DiffCtx, f1, f2)
   (isempty(f1) || isempty(f2)) && return
   (label(f1[1]) == label(f2[1])) && return
-  fdiff(f1[2:end], f2[2:end]) + Patch([Replace(f1[1], f2[1])])
+  fdiff(cx, f1[2:end], f2[2:end]) + Patch([Replace(f1[1], f2[1])])
 end
 
-function modify(f1, f2)
+function modify(cx::DiffCtx, f1, f2)
   (isempty(f1) || isempty(f2)) && return
   label(f1[1]) == label(f2[1]) || return
-  fdiff(children(f1[1]), children(f2[1])) +
-    fdiff(f1[2:end], f2[2:end])
+  fdiff(cx, children(f1[1]), children(f2[1])) +
+    fdiff(cx, f1[2:end], f2[2:end])
 end
 
-function fdiff(f1, f2)
+function fdiff(cx::DiffCtx, f1, f2)
   isempty(f1) && isempty(f2) && return Patch([])
-  best([f(f1, f2) for f in [replace, insert, delete, modify]])
+  haskey(cx.cache, (f1, f2)) && return cx.cache[(f1, f2)]
+  cx.cache[(f1, f2)] = best([f(cx, f1, f2) for f in [replace, insert, delete, modify]])
 end
 
-diff(x1, x2) = fdiff([x1], [x2])
+diff(x1, x2) = fdiff(DiffCtx(), [x1], [x2])
