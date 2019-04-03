d300379e5824c54443185a462b7a43f1b31771cb
Mike J Innes <mike.j.innes@gmail.com>
2019-04-03 16:02:06 +0100

    rework docs build

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,11 @@
 using Documenter, MacroTools
 
 makedocs(
-    modules = [MacroTools],
-    format  = :html,
     sitename = "MacroTools",
-    pages = Any[
-        "Introduction to MacroTools"   => "index.md"
-    ],
-    # Use clean URLs, unless built as a "local" build
-    html_prettyurls = !("local" in ARGS),
-    # html_canonical = "https://juliadocs.github.io/Documenter.jl/latest/",
-)
+    pages = [
+        "Home" => "index.md"],
+    format=:html)
 
 deploydocs(
-    repo = "MikeInnes/MacroTools.jl.git",
-    target = "build",
-    julia = "1.0",
-)
+    repo = "github.com/MikeInnes/MacroTools.jl.git",
+    julia="1.0")
