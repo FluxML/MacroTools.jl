dcf8b46361290c28de4ff5cfac01fc06fc7505ef
David Widmann <devmotion@users.noreply.github.com>
2025-01-05 22:17:40 +0100

    Remove test with StableRNG

--- test/utils.jl
+++ test/utils.jl
@@ -1,14 +1,11 @@
 using MacroTools: isdef, flatten, striplines, @qq
 
-import StableRNGs
-import Random
-
 @testset "animals" begin
     @test MacroTools.animals isa Vector{Symbol}
     @test length(MacroTools.animals) == 214
     @test allunique(MacroTools.animals)
     @test all(islowercase ∘ first ∘ string, MacroTools.animals)
-    @test MacroTools.animals == Random.shuffle!(StableRNGs.StableRNG(1234), sort(MacroTools.animals))
+    @test !issorted(MacroTools.animals)
 end
 
 @testset "utils" begin
